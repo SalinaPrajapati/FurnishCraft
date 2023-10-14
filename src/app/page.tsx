f008f723d5985fe06221ecf1b81419396ef72777
SalinaPrajapati <saleenaprajapati1@gmail.com>
2023-10-15 00:26:51 +0545

    add collection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
+import Collection from "@/components/Collection"
 
 export default function HomePage() {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Collection></Collection>
     </>
   )
 }
---
+++ src/components/Collection.tsx
@@ -0,0 +1,71 @@
+
+const products = [
+    {
+      id: 1,
+      name: 'Home Decore',
+      href: '#',
+      imageSrc: '/assets/chair-2.jpg',
+      imageAlt: "Picture is not given",
+      item: '8 items'
+    },
+    {
+      id: 1,
+      name: 'Home Decore',
+      href: '#',
+      imageSrc: '/assets/sofa.jpg',
+      imageAlt: "Picture is not given",
+      item: '8 items'
+    },
+    {
+      id: 1,
+      name: 'Home Decore',
+      href: '#',
+      imageSrc: '/assets/chair.jpg',
+      imageAlt: "Picture is not given.",
+      item: '8 items'
+    },
+    {
+      id: 1,
+      name: 'Home Decore',
+      href: '#',
+      imageSrc: '/assets/table-1.jpg',
+      imageAlt: "Picture is not given.",
+      item: '8 items'
+    },
+    // More products...
+  ]
+  
+  export default function Collection() {
+    return (
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl py-16 sm:py-20 lg:max-w-7xl">
+          <h2 className="text-2xl font-bold text-center tracking-tight text-gray-900">Our Collection</h2>
+          <div className="border-b-2 border-yellow-600 w-24 text-center m-auto"></div>
+  
+          <div className="mt-12 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+              <div key={product.id} className="group relative">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80 relative">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                  <div className="absolute bottom-0 flex justify-between left-0 right-0 p-3 m-2 bg-white transition duration-300 hover:bg-black">
+                    <h3 className="text-sm text-gray-700 hover:text-white">
+                      <a href={product.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {product.name}
+                      </a>
+                    </h3>
+                    <p className="text-gray-700 text-sm hover:text-white">
+                        ({product.item})</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
