5984184d5108dc15b8c0eae2d999975cba21981f
salinaprajapati <saleenaprajapati1@gmail.com>
2024-02-05 17:37:38 +0545

    add product list

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,40 +1,52 @@
 "use client";
 
 import Timer from "@/components/Timer";
-import { HiShoppingCart } from "react-icons/hi";
+import { HiShoppingCart, HiHeart } from "react-icons/hi";
 import { useState } from "react";
+import { products } from "@/utils/product";
+
 export default function Product() {
   const [isHovered, setIsHovered] = useState(false);
 
   const myStyle = {
     bottom: "30.2rem",
+    top: "-11px",
+    height: "30px",
   };
   return (
-    <div className="relative m-14 p-9 border-solid border-2 border-sky-500">
+    <div className="relative m-14 p-9 h-1/3 border-solid border-2 border-sky-500">
       <div className="absolute left-6" style={myStyle}>
         <Timer></Timer>
       </div>
-      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        <div
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <img
-            src={isHovered ? "/assets/sofa.jpg" : "/assets/chair-2.jpg"}
-            className="transition ease-in-out hover:delay-150 hover:duration-300 hover:transition-all hover:scale-110"
-          />
-        </div>
-        <div className="flex justify-between w-20 h-12 border-solid border-2 border-sky-500 items-center">
-            <span className="border-r-2 border-sky-500 h-full w-10 flex justify-around items-center">
-              <HiShoppingCart style={{ fontSize: "24px" }} />
-            </span>
-            <span className="h-full w-10 flex justify-around items-center">
-              <HiShoppingCart style={{ fontSize: "24px" }} />
-            </span>
-        </div>
-        {/* <div>
-          <img src="/assets/sofa.jpg" className="" />
-        </div> */}
+      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8 h-auto">
+        {products.map((product: any, index: number) => (
+          <div key={index} className="h-auto w-full inline-block align-middle">
+            <div
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              className="max-w-[18rem]"
+            >
+              <div className="max-w-[18rem] h-2/6 inline-block align-middle relative overflow-hidden bg-cover bg-no-repeat">
+                <img
+                  src={
+                    isHovered
+                      ? product.images[0].image
+                      : product.images[0].picture
+                  }
+                  className="transition ease-in-out hover:delay-150 hover:duration-300 hover:transition-all hover:scale-110 object-cover h-full w-full m-auto"
+                />
+              </div>
+            </div>
+            <div className="flex justify-between mt-2 p-2 border-solid border-2 w-1/3 m-auto border-sky-500">
+              <span className="w-1/2 flex justify-center items-center">
+                <HiShoppingCart style={{ fontSize: "24px" }} />
+              </span>
+              <span className="w-1/2 flex justify-center items-center">
+                <HiHeart style={{ fontSize: "24px" }} />
+              </span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
